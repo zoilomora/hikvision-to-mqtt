8a490c4ac6b08ef29fe9fe647a9e030eebeda7d4
Zoilo Mora <zoilo.mora@hotmail.com>
2020-01-06 14:49:06 +0100

    Infrastructure adjustments and fix disconnection by exceeded timeout

--- src/Infrastructure/Domain/Service/Event/MqttPublisher.php
+++ src/Infrastructure/Domain/Service/Event/MqttPublisher.php
@@ -10,18 +10,31 @@ use ZoiloMora\HikvisionCommunicator\Domain\Service\Event\Publisher;
 final class MqttPublisher implements Publisher
 {
     private Client $client;
+    private string $broker;
+    private int $port;
     private string $topic;
 
-    public function __construct(Client $client, string $topic)
+    public function __construct(Client $client, string $broker, int $port, string $topic)
     {
         $this->client = $client;
+        $this->broker = $broker;
+        $this->port = $port;
         $this->topic = $topic;
     }
 
     public function execute(Event $event): void
     {
         $message = \json_encode($event);
+
+        // https://mosquitto-php.readthedocs.io/en/latest/client.html#Mosquitto\Client::connect
+        $this->client->connect($this->broker, $this->port);
+
+        // https://mosquitto-php.readthedocs.io/en/latest/client.html#Mosquitto\Client::publish
         $this->client->publish($this->topic, $message);
+
+        // https://mosquitto-php.readthedocs.io/en/latest/client.html#Mosquitto\Client::disconnect
+        $this->client->disconnect();
+
         echo sprintf("Published message: %s\n", $message);
     }
 }
