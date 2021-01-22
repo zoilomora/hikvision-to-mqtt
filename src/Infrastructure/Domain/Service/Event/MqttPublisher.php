b87ca7c59848cb7f467b6d78d824a3e001893adb
Zoilo Mora <zoilo.mora@hotmail.com>
2021-01-22 21:40:59 +0100

    Allow login in MQTT Server

--- src/Infrastructure/Domain/Service/Event/MqttPublisher.php
+++ src/Infrastructure/Domain/Service/Event/MqttPublisher.php
@@ -12,19 +12,34 @@ final class MqttPublisher implements Publisher
     private Client $client;
     private string $broker;
     private int $port;
+    private string $username;
+    private string $password;
     private string $topic;
 
-    public function __construct(Client $client, string $broker, int $port, string $topic)
-    {
+    public function __construct(
+        Client $client,
+        string $broker,
+        int $port,
+        string $username,
+        string $password,
+        string $topic
+    ) {
         $this->client = $client;
         $this->broker = $broker;
         $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
         $this->topic = $topic;
     }
 
     public function execute(Event $event): void
     {
         $message = \json_encode($event);
+
+        if ('' !== $this->username && '' !== $this->password) {
+            // https://mosquitto-php.readthedocs.io/en/latest/client.html#Mosquitto\Client::setCredentials
+            $this->client->setCredentials($this->username, $this->password);
+        }
 
         // https://mosquitto-php.readthedocs.io/en/latest/client.html#Mosquitto\Client::connect
         $this->client->connect($this->broker, $this->port);
